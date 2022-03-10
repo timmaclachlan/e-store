154603529908fb960202ed879df7fbe2f02d8dd6
MEYHOUSE\Tim.MacLachlan <tim.maclachlan@morningdata.co.uk>
2022-03-10 18:08:59 +0000

    adding context

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import CartContextProvider from "./contexts/cartContext";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <CartContextProvider>
+            <App />
+        </CartContextProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
-
-
